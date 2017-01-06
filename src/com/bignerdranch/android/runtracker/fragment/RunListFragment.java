8e393f9faaba56d9fb22d64e6231ada31b646bcf
jacrylodai-woshi <jacrylodai@sina.com>
2017-01-06 18:38:33 +0800

    *添加新的旅程

--- src/com/bignerdranch/android/runtracker/fragment/RunListFragment.java
+++ src/com/bignerdranch/android/runtracker/fragment/RunListFragment.java
@@ -1,34 +1,90 @@
 package com.bignerdranch.android.runtracker.fragment;
 
+import java.util.Date;
+
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v4.widget.CursorAdapter;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.bignerdranch.android.runtracker.R;
+import com.bignerdranch.android.runtracker.activity.RunActivity;
 import com.bignerdranch.android.runtracker.db.RunDatabaseHelper.RunCursor;
 import com.bignerdranch.android.runtracker.domain.Run;
 import com.bignerdranch.android.runtracker.manager.RunManager;
 
 public class RunListFragment extends ListFragment {
+	
+	private static final String TAG = "RunListFragment";
+	
+	private static final int REQUEST_CODE_NEW_RUN = 1;
 
 	private RunCursor mRunCursor;
+	
+	private RunCursorAdapter mAdapter;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		
+		setHasOptionsMenu(true);
+		
 		super.onCreate(savedInstanceState);		
 		mRunCursor = RunManager.getInstance(getActivity()).queryRunList();
 		
-		RunCursorAdapter adapter = new RunCursorAdapter(getActivity(), mRunCursor);
-		setListAdapter(adapter);
+		mAdapter = new RunCursorAdapter(getActivity(), mRunCursor);
+		setListAdapter(mAdapter);
 	}
 	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.run_list_options, menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch (item.getItemId()) {
+		case R.id.menu_item_new_run:
+			
+			Intent intent = new Intent(getActivity(),RunActivity.class);
+			startActivityForResult(intent, REQUEST_CODE_NEW_RUN);
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		
+		Log.i(TAG,"onActivityResult");
+		Log.i(TAG,"resultCode:"+resultCode);
+		switch (requestCode) {
+		case REQUEST_CODE_NEW_RUN:
+			
+			Log.i(TAG, "requery");
+			mRunCursor.requery();
+			mAdapter.notifyDataSetChanged();
+			break;
+
+		default:
+			break;
+		}
+	}
+		
 	@Override
 	public void onDestroy() {
 		
@@ -51,7 +107,11 @@ public class RunListFragment extends ListFragment {
 			Run run = mCursor.getRun();
 			
 			TextView tvStartDate = (TextView)view;
-			String cellText = context.getString(R.string.run_desc, run.getStartDate());
+			Date startDate = run.getStartDate();
+			String startDateStr = 
+					DateFormat.getMediumDateFormat(context).format(startDate) + " " +
+					DateFormat.getTimeFormat(context).format(startDate);
+			String cellText = context.getString(R.string.run_desc, startDateStr);
 			tvStartDate.setText(cellText);
 		}
 
